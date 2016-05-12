59ed670ca28da8ff1c769b220072b904ee8ad689
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-05-12 02:10:14 -0500

    new packages required

--- server.js
+++ server.js
@@ -5,3 +5,20 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree or translated in the assets folder.
  */
+
+/**
+ * Module dependencies
+ */
+var express             = require('express'),
+    bodyParser          = require('body-parser'),
+    errorHandler        = require('errorhandler'),
+    favicon             = require('serve-favicon'),
+    hbs                 = require('express-hbs'),
+    methodOverride      = require('method-override'),
+    moment              = require('moment'),
+    path                = require('path'),
+    session             = require('express-session'),
+    passport            = require('passport'),
+
+    logger              = require('./config/logger').logger,
+    morgan              = require('morgan'),
