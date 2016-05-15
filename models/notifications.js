bd1ecffd6a8ec99c9844215c8a8d7db08d3c3558
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-05-14 23:03:16 -0500

    notification models, will change the structure

---
+++ models/notifications.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2016-present, Agoo.com.co <http://www.agoo.com.co>.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree or translated in the assets folder.
+ */
+
+// Load required packages
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// Define our notifications schema
+var NotificationSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    creationDate: Date,
+    enabled: Boolean
+},{ versionkey: false});
+
+// Export the Mongoose model
+module.exports.Notification = mongoose.model('Notification', NotificationSchema);
