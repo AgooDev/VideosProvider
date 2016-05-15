fcc482d7e85fb40b52eceac54b5b1fcb5e890351
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-05-15 12:03:44 -0500

    cleaning user routes

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,7 @@
 // Load required packages
 var logger = require('../config/logger').logger;
 var User = require('../models/users').Users;
+var UserReset = require('../models/userReset').UserReset;
 
 // ENDPOINT: /users METHOD: GET
 exports.getUsers = function(req, res){
@@ -33,7 +34,7 @@ exports.getUserById = function(req, res){
     var initialDate = req.query.initialDate;
     var endDate = req.query.endDate;
     // COUNT ENDPOINT CALLED
-    if (req.params.id == 'count'){
+    if (req.params.id === 'count'){
         // DATE FILTER
         if( (!(typeof initialDate === 'undefined')) && (!(typeof endDate === 'undefined')) ){
             User.count({ 'creationDate' : { '$gte' : initialDate , '$lt' : endDate } }, function (err, countDateUser) {
@@ -43,8 +44,8 @@ exports.getUserById = function(req, res){
                     res.send(err);
                 }
                 // Success
-                res.json({ message:"The count of users between dates", initialDate: initialDate, endDate: endDate
-                    , data: countDateUser });
+                res.json({ message:'The count of users between dates', initialDate: initialDate,
+                    endDate: endDate, data: countDateUser });
             });
             return;
         }else{
@@ -55,7 +56,7 @@ exports.getUserById = function(req, res){
                     res.send(err);
                 }
                 // Success
-                res.json({ message:"The complete count of users", data: countUser });
+                res.json({ message:'The complete count of users', data: countUser });
             });
         }
         return;
@@ -85,7 +86,7 @@ exports.getLogin = function (req, res) {
             return;
         }
         // success
-        res.json({ message:"Login authenticated successfully", data: user });
+        res.json({ message:'Login authenticated successfully', data: user });
     });
 };
 
@@ -99,7 +100,6 @@ exports.postPasswordReset = function (req, res) {
         if(err){
             logger.error(err);
             res.json({ message: 'Email provided doesn`t Exists'});
-            //res.send(err);
             return;
         }
 
@@ -118,96 +118,8 @@ exports.postPasswordReset = function (req, res) {
         var currentYear = new Date().getFullYear();
         //logger.info(currentYear);
 
-        // Get sent_at date
-        //var sendAtDate = '2015-01-01 12:12:12';
-        //logger.info(sendAtDate);
 
-        // Send the email
-        //logger.info(emailRequest);
-        //logger.info(global.MANDRILLKEY);
-        mandrillClient = new mandrill.Mandrill(global.MANDRILLKEY);
-        var templateName = "recuperar";
-        var template_content = [{
-            "name": "CODIGO",
-            "content": codeResult
-        },{
-            "name": "CURRENT_YEAR",
-            "content": currentYear
-        },{
-            "name": "USEREMAIL",
-            "content": emailRequest
-        }
-        ];
-        var message = {
-            "subject": "Recuperar Contraseña Estoy Bien App",
-            "from_email": "soporte@estoybien.co",
-            "from_name": "Soporte Estoy Bien App",
-            "to": [{
-                "email": emailRequest,
-                "name": fullName,
-                "type": "to"
-            }],
-            "merge_vars": [{
-                "rcpt": emailRequest,
-                "vars": [{
-                    "name": "CODIGO",
-                    "content": codeResult
-                },{
-                    "name": "CURRENT_YEAR",
-                    "content": currentYear
-                },{
-                    "name": "USEREMAIL",
-                    "content": emailRequest
-                }]
-            }]
-        };
-        var async = false;
-        var ip_pool = "Main Pool";
-        //var send_at = sendAtDate;
-        mandrillClient.messages.sendTemplate({"template_name": templateName, "template_content": template_content,
-            "message": message, "async": async, "ip_pool": ip_pool}, function(result) {
-            //logger.info(result);
-            //logger.info(result[0].status);
-            var estado = result[0].status;
-            var razonRechazo = result[0].reject_reason;
-            if(estado == 'sent'){
-                var saveReset = new UserReset();
-                saveReset.name = fullName;
-                saveReset.idUser = idUser;
-                saveReset.email = emailRequest;
-                saveReset.code = codeResult;
-
-                saveReset.save(function(err){
-                    // Check for errors and show message
-                    if(err){
-                        logger.error(err);
-                        res.send(err);
-                    }
-                    // success
-                    // success Email sent and user email validated
-                    //logger.info(JSON.stringify(saveReset));
-                    res.json({ message: 'Email sent successfully!' });
-                });
-
-            }else{
-
-                // ERROR Email sent and user email validated
-                res.json({ message: 'Error at send Email!', reason: razonRechazo });
-            }
-            /*
-             [{
-             "email": "recipient.email@example.com",
-             "status": "sent",
-             "reject_reason": "hard-bounce",
-             "_id": "abc123abc123abc123abc123abc123"
-             }]
-             */
-        }, function(e) {
-            // Mandrill returns the error as an object with name and message keys
-            console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
-            // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
-        });
-
+        // TODO: Agregar soporte para restaurar la contraseña, para externos y para el servicio
     });
 };
 
@@ -264,58 +176,9 @@ exports.PatchPasswordReset = function (req, res) {
                 });
             });
 
-            /*
-            // Password changed so we need to hash it
-            bcrypt.genSalt(5, function (err, salt) {
-                // Check for errors and show message
-                if(err){
-                    logger.error(err);
-                    res.send(err);
-                    return;
-                }
-
-                bcrypt.hash(passRequest, salt, null, function (err, hash) {
-                    // Check for errors and show message
-                    if(err){
-                        logger.error(err);
-                        res.send(err);
-                        return;
-                    }
-
-                    logger.info(hash);
-
-                    user.password = JSON.stringify(hash);
-                    //user.password = hash;
-
-                    user.save(function(err){
-                        // Check for errors and show message
-                        if(err){
-                            logger.error(err);
-                            res.send(err);
-                            return;
-                        }
-                        // Now deleted the code from the collection
-                        UserReset.findByIdAndRemove(idUserRequest, function(err){
-                            // Check for errors and show message
-                            if(err){
-                                logger.error(err);
-                                res.send(err);
-                                return;
-                            }
-                            // success
-                            // success
-                            res.json({ message: 'User password changed and code: ' + codeRequest +' deleted successfully!' });
-                        });
-                    });
-                });
-            });
-            */
-
         });
     });
 };
-
-
 
 // ENDPOINT: /users METHOD: POST
 exports.postUser = function (req, res) {
@@ -327,9 +190,6 @@ exports.postUser = function (req, res) {
     user.lastName = req.body.lastName;
     user.email = req.body.email;
     user.password = req.body.password;
-    // TODO: rectificar insercion de array de objetos
-    //user.rol = req.body.rol;
-    //user.permissions = req.body.permissions;
     user.creationDate = Date.now();
     user.enabled = true;
 
@@ -362,9 +222,6 @@ exports.putUser = function(req, res){
         user.password = req.body.password;
         user.creationDate = req.body.creationDate;
         user.enabled = req.body.enabled;
-        // TODO: rectificar insercion de array de objetos
-        //user.rol = req.body.rol;
-        //user.permissions = req.body.permissions;
         user.save(function(err){
             // Check for errors and show message
             if(err){
