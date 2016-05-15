094ae74494d94d8064669e73fc0d9ea62cd267a6
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-05-14 23:03:46 -0500

    public videos available for everyone that download and install the app

---
+++ models/publicVideos.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2016-present, Agoo.com.co <http://www.agoo.com.co>.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree or translated in the assets folder.
+ */
+
+// Load required packages
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// Define our public videos schema
+var PublicVideoSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    creationDate: Date,
+    enabled: Boolean
+},{ versionkey: false});
+
+// Export the Mongoose model
+module.exports.PublicVideos = mongoose.model('PublicVideo', PublicVideoSchema);
