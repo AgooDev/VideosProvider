71f10cfa7becb5609ffa284532a2a72085b0f0b8
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-05-17 14:44:09 -0500

    relocating code

--- routes/users.js
+++ routes/users.js
@@ -75,6 +75,105 @@ exports.getUserById = function(req, res){
     });
 };
 
+// ENDPOINT: /users METHOD: POST
+exports.postUser = function (req, res) {
+    // Create a new instance of the User model
+    var user = new User();
+
+    // Set the User properties that came from the POST data
+    user.name = req.body.name;
+    user.lastName = req.body.lastName;
+    user.email = req.body.email;
+    user.password = req.body.password;
+    user.creationDate = Date.now();
+    user.enabled = true;
+
+    user.save(function(err){
+        // Check for errors and show message
+        if(err){
+            logger.error(err);
+            res.send(err);
+            return;
+        }
+        //Success
+        res.json({ message: 'User created successfully!', data: user });
+    });
+};
+
+// ENDPOINT: /users/:id METHOD: PUT
+exports.putUser = function(req, res){
+    User.findById(req.params.id, function (err, user) {
+        // Check for errors and show message
+        if(err){
+            logger.error(err);
+            res.send(err);
+            return;
+        }
+
+        // Set the User properties that came from the PUT data
+        user.name = req.body.name;
+        user.lastName = req.body.lastName;
+        user.email = req.body.email;
+        user.password = req.body.password;
+        user.creationDate = req.body.creationDate;
+        user.enabled = req.body.enabled;
+        user.save(function(err){
+            // Check for errors and show message
+            if(err){
+                logger.error(err);
+                res.send(err);
+            }
+            // success
+            res.json({message: 'User updated successfully', data: user });
+        });
+    });
+};
+
+// ENDPOINT: /users/:id METHOD: PATCH
+exports.patchUser = function(req, res){
+    User.findById(req.params.id, function (err, user) {
+        // Check for errors and show message
+        if(err){
+            logger.error(err);
+            res.send(err);
+            return;
+        }
+
+        user.enabled = req.body.enabled;
+
+        user.save(function(err){
+            // Check for errors and show message
+            if(err){
+                logger.error(err);
+                res.send(err);
+                return;
+            }
+            var message = '';
+            if(user.enabled === true){
+                message = 'User enabled successfully';
+            }else{
+                message = 'User disbled successfully';
+            }
+            // success
+            res.json({message: message, data: user });
+        });
+    });
+};
+
+// ENDPOINT: /users/:id METHOD: DELETE
+exports.deleteUser = function(req, res){
+    User.findByIdAndRemove(req.params.id, function(err){
+        // Check for errors and show message
+        if(err){
+            logger.error(err);
+            res.send(err);
+            return;
+        }
+        // success
+        res.json({ message: 'User deleted successfully!' });
+    });
+};
+
 // ENDPOINT: /login METHOD: GET
 exports.getLogin = function (req, res) {
     // Use the 'User' model to find all users
